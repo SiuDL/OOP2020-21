14a696a91b8caf3cd24d9dadfca5d9e61dbae299
SiuDzoen <C19401596@mytudublin.ie>
2020-10-02 11:07:49 +0100

    my lab2 solution

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -15,23 +15,30 @@ class Types_and_Strings:
 
         #
         # Enter your own print statements below:
-        #
+        print("Hello, this is my second OOP lab.")
 
         # print only first and last of the sentence:
-
+        print(message[0])
+        print(message[-1])
 
         # use slice notation:
-
+        statement = "message"
+        print(statement[:1])
+        print(statement[6:7])
 
         # escaping a character:
-
+        print("he said \"that\'s fantastic\"!")
 
         # find all a's in the input word and count how many there are:
-
+        print(statement.find('a'))
+        print(statement.count('a'))
 
         # replace all occurences of the character a with the - sign
+        print("This is another tribute to a vergil".replace('a','-'))
+
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace():
+        print("This is another tribute to a vergil".replace('a','-',1))
 
 
     def play_with_lists(self):
@@ -39,16 +46,21 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out:
-
+        myList = (list(message.split()))
+        print(myList)
 
         # append a new element to the list and print:
-
+        myList.append(" yes")
+        print(myList)
 
         # remove from the list in 3 ways:
-
+        del myList[3]
+        print(myList)
+        myList.remove(' yes')
+        print(myList)
 
         # check if the word cake is in your input list:
-
+        print("cake" in myList)
 
         # reverse the items in the list and print:
 
@@ -61,5 +73,5 @@ class Types_and_Strings:
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+#tas.play_with_strings()
+tas.play_with_lists()
